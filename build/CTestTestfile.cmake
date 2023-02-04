203e02a1b4b6be1523a5b9891ee2ac14325f322f
Andrew Zhang <andrewzhang1000@gmail.com>
2023-02-04 12:40:09 -0500

    Andrew's Random Walk Algorithm

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chris/mie443_ws/src
-# Build directory: /home/chris/mie443_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(mie443_contest1)
